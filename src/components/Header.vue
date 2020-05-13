a715773fb0ed0be100cfceb9810d9a2c7eea6eed
Anders <anders_thy@rocketmail.com>
2020-05-13 09:35:23 +0200

    something

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+    <header>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <router-link to="/" class="navbar-item">
+                    <img
+                        src="../assets/images/vaskeLogo.png"
+                        alt="Vaske logo"
+                    />
+                </router-link>
+
+                <a
+                    role="button"
+                    class="navbar-burger-trigger"
+                    @click="showMenu()"
+                >
+                    <span class="navbar-burger-span"></span>
+                    <span class="navbar-burger-span"></span>
+                    <span class="navbar-burger-span"></span>
+                </a>
+            </div>
+
+            <div class="navbar-menu" :class="show ? 'show' : ''">
+                <div class="navbar-end">
+                    <router-link to="/about" class="navbar-item">
+                        About
+                    </router-link>
+
+                    <router-link to="/about" class="navbar-item">
+                        About
+                    </router-link>
+
+                    <router-link to="/about" class="navbar-item">
+                        About
+                    </router-link>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Header extends Vue {
+    public show = false
+
+    showMenu() {
+        this.show = !this.show
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/components/header.scss';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './../node_modules/bulma/bulma.sass'
+import './assets/styles/global.scss'
 
-Vue.config.productionTip = false;
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app')
